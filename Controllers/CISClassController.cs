55ab74dff3037009467882801ebca80073139963
Matthew Shan <matthewshan99@gmail.com>
2020-01-02 20:15:57 -0500

    Initial Commit

---
+++ Controllers/CISClassController.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CISClassController : ControllerBase
+    {
+        private Dictionary<String, CISClass> CISClasses = new Dictionary<String, CISClass>();
+
+        public CISClassController() {
+            CISClasses.Add("162-1", 
+                new CISClass
+                {
+                    ClassCode = 162,
+                    Section = 1,
+                    Instructor = "Professor Bob",
+                    Prereqs = new int[] {}
+                });
+            CISClasses.Add("162-2", 
+                new CISClass
+                {
+                    ClassCode = 162,
+                    Section = 2,
+                    Instructor = "Professor Rob",
+                    Prereqs = new int[] {}
+                });
+            CISClasses.Add("163-1", 
+                new CISClass
+                {
+                    ClassCode = 163,
+                    Section = 1,
+                    Instructor = "Professor Joe",
+                    Prereqs = new int[] {162}
+                });
+        }
+
+        [HttpGet]
+        public CISClass[] GetAll()
+        {
+            return this.CISClasses.Values.ToArray();
+        }
+
+        [HttpGet("ClassID")]
+        public CISClass GetClass(string ClassID)
+        {
+            return this.CISClasses[ClassID];
+        }
+
+        [HttpPost]
+        public CISClass[] AddClass([FromBody] CISClass classModel)
+        {
+            String classId = classModel.ClassCode + "-" + classModel.Section;
+            this.CISClasses.Add(classId, classModel);
+            return this.GetAll();
+        }
+    }
+}
+/*
+        [HttpPost]
+        public ActionResult Post([FromBody] CISClassClass class)
+        {
+            String classId = class.ClassCode + "-" + class.Section;
+            CISClasses.Add(classId, class);
+            return Ok();
+        }
+*/
---
+++ Models/CISClass.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyAPI
+{
+    public class CISClass {
+        public int ClassCode {get; set;}
+        public int Section {get; set;}
+        public String Instructor {get; set;}
+        public int[] Prereqs {get; set;}
+    }
+}
